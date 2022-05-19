ad5167517baacb37385e00b06ba6d68c4e9fbb75
JRM <johnrhmartin@gmail.com>
2022-05-19 17:39:39 -0400

    updated resume page to add a downloadable version of my resume

--- src/routes/resume.jsx
+++ src/routes/resume.jsx
@@ -1,11 +1,23 @@
+import React from 'react';
 import { Footer } from '../footer';
 import { Header } from '../header';
+import resume from '../assets/CV_JM.pdf';
+
+class DownloadLink extends React.Component {
+  render() {
+      return (
+          <a href={this.props.src} download>{this.props.children}</a>
+      )
+  }
+}
+
 export default function Resume() {
     return (
       <>
       <Header />
       <div className="App">
         <h2>Resume</h2>
+        <DownloadLink src={resume}>Resume</DownloadLink>
       </div>
       <Footer />
       </>
