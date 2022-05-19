d551cd6a19cfd1c0ca4e8a4b272107a66be1a913
JRM <johnrhmartin@gmail.com>
2022-05-19 17:53:20 -0400

    updated the resume page to include fron-end and back-end proficiencies

--- src/routes/resume.jsx
+++ src/routes/resume.jsx
@@ -18,6 +18,30 @@ export default function Resume() {
       <div className="App">
         <h2>Resume</h2>
         <DownloadLink src={resume}>Resume</DownloadLink>
+        <h3>Front-end Proficiences</h3>
+        <ul>
+          <li>React</li>
+          <li>Javascript</li>
+          <li>HTML</li>
+          <li>Netlify</li>
+          <li>Photoshop</li>
+        </ul>
+        <h3>Back-end Proficiences</h3>
+        <ul>
+          <li>Node.js</li>
+          <li>MongoDB</li>
+          <li>Mongoose ODM</li>
+          <li>MySQL2</li>
+          <li>Sequelize</li>
+          <li>NPM Packages</li>
+          <ul>
+            <li>Express.js</li>
+            <li>Inquirer</li>
+            <li>Console.table</li>
+          </ul>
+          <li>Heroku / Heroku CLI</li>
+          <li>Insomnia</li>
+        </ul>
       </div>
       <Footer />
       </>
