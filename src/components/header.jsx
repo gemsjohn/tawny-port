e8996a859ccf68a7dfccb52230f5b8d4d2ece41d
JRM <johnrhmartin@gmail.com>
2022-05-18 21:41:27 -0400

    installed react-bootstrap and created a navigation bar

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -1,8 +1,19 @@
+import Navbar from 'react-bootstrap/Navbar';
+import Container from 'react-bootstrap/Container';
+import Nav from 'react-bootstrap/Nav';
 
 export const Header = () => {
     return (
-        <div className="App-header">
-            <h1>Header</h1>
-        </div>
+            <Navbar bg="dark" variant="dark">
+                <Container>
+                <Navbar.Brand href="#home">John Martin</Navbar.Brand>
+                <Nav className="me-auto">
+                <Nav.Link href="#about">About Me</Nav.Link>
+                <Nav.Link href="#portfolio">Portfolio</Nav.Link>
+                <Nav.Link href="#contact">Contact</Nav.Link>
+                <Nav.Link href="#resume">Resume</Nav.Link>
+                </Nav>
+                </Container>
+            </Navbar>
     )
 }
