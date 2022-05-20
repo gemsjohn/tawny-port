8e45f39384d191b94488c5b3ecb165e3e036bcd6
JRM <johnrhmartin@gmail.com>
2022-05-20 13:00:25 -0400

    updated the About Me page

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import './App.css';
-import { Container, Card, Button } from 'react-bootstrap';
-import photo from './assets/favicon.png';
+import { Container, Card, Col, Row } from 'react-bootstrap';
+import photo from './assets/headshot.png';
 import { Header } from './header';
 import { Footer } from './footer';
 
@@ -21,17 +21,25 @@ function App() {
         <Card className="extra-spacing" style={styles.card}>
           <Card.Header as="h5" style={styles.card}>About Me</Card.Header>
           <Card.Body>
-            <img src={photo} className="profile" alt="" />
-            <h5> 
-              I'm a hobbiest developer born and raised in North Carolina. I currently work at WCG VeraSci where our company has produced a Browser, Tablet, and Phone
-              based application called Pathway, which is designed to evaluate cognitive disorders. My role at the compnay is to Manage a Technology Quality Assurance Team 
-              that ensures regulatory compliance. My team falls within the Software Development Life Cycle (SDLC), right behind our internal Development Team.
-            </h5>
-            <h5>
-              On a personal level I enjoy restoring old Rally Cars or anything rare. Love spending time with my Wife, Dogs, and Cat. I'll try anything at least once and therefore
-              enjoy checking out new restaurants.  
-            </h5>
-
+            
+            <Row>
+              <Col sm={3}>
+                <img src={photo} className="profile" alt="" />
+              </Col>
+              <Col sm={9}>
+              <div className="extra-spacing"></div>
+                <a> 
+                  I'm a hobbiest developer born and raised in North Carolina. I currently work at WCG VeraSci where our company has produced a Browser, Tablet, and Phone
+                  based application called Pathway, which is designed to evaluate cognitive disorders. My role at the compnay is to Manage a Technology Quality Assurance Team 
+                  that ensures regulatory compliance. My team falls within the Software Development Life Cycle (SDLC), right behind our internal Development Team.
+                </a>
+                <div className="extra-spacing"></div>
+                <a>
+                  On a personal level I enjoy restoring old Rally Cars or anything rare. Love spending time with my Wife, Dogs, and Cat. I'll try anything at least once and therefore
+                  enjoy checking out new restaurants.  
+                </a>
+              </Col>
+            </Row>
           </Card.Body>
         </Card>
         </Container>
