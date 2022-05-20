dee6365fd4270a346c358a0697fc854ca2a7b38d
JRM <johnrhmartin@gmail.com>
2022-05-19 22:20:55 -0400

    updated the contact page fix the submit button. no the button has an href={mailto}

--- src/routes/contact.jsx
+++ src/routes/contact.jsx
@@ -29,99 +29,66 @@ export default function Contact() {
       <div className="App">
         <h3 className="extra-spacing">Contact Me!</h3>
       <Formik
-      validationSchema={schema}
-      initialValues={{
-          name: '',
-          email: '',
-          message: ''
-        }}
-      >
-        {({
-          handleChange,
-          values,
-          touched,
-          errors,
-        }) => (
-          <Form noValidate validated={validated} onSubmit={handleSubmit}>
-              <Form.Group className="mb-3" as={Col} md="4" controlId="validationFormik01">
-                <Form.Label>Name</Form.Label>
-                <Form.Control
-                  type="text"
-                  name="name"
-                  placeholder="Enter name"
-                  value={values.name}
-                  onChange={handleChange}
-                  // isValid={touched.name && !errors.name}
-                  isInvalid={!!errors.name}
-                />
-              </Form.Group>
-              <Form.Group className="mb-3" as={Col} md="4" controlId="validationFormik02">
-                <Form.Label>Email</Form.Label>
-                <Form.Control
-                  type="email"
-                  name="email"
-                  placeholder="Email address"
-                  value={values.email}
-                  onChange={handleChange}
-                  isInvalid={!!errors.email}
-                />
-                 <Form.Control.Feedback type="invalid">
-                  {errors.email}
-                </Form.Control.Feedback>
-              </Form.Group>
-              <Form.Group className="mb-3" as={Col} md="4" controlId="validationFormik03">
-                <Form.Label>Message</Form.Label>
-                <Form.Control
-                  type="text"
-                  name="message"
-                  placeholder="Enter message"
-                  value={values.message}
-                  onChange={handleChange}
-                  isInvalid={!!errors.message}
-                />
-                 <Form.Control.Feedback type="invalid">
-                  {errors.message}
-                </Form.Control.Feedback>
-              </Form.Group>
-              <Button variant="primary" type="submit">
-                Submit
-              </Button>
-          </Form>
-        )}
-    </Formik>
-        {/* <Form noValidate validated={validated} onSubmit={handleSubmit}>
-          <Form.Group className="mb-3" controlId="formBasicEmail">
-            <Form.Label>Name</Form.Label>
-            <Form.Control 
-              required
-              type="text" 
-              placeholder="Enter name" 
-            />
-            <Form.Control.Feedback type="invalid">
-              Please enter your name.
-            </Form.Control.Feedback>
-          </Form.Group>
-          <Form.Group className="mb-3" controlId="formBasicEmail">
-            <Form.Label>Email address</Form.Label>
-            <Form.Control 
-              required
-              type="email" 
-              placeholder="Enter email" 
-            />
-          </Form.Group>
-
-          <Form.Group className="mb-3" controlId="formBasicPassword">
-            <Form.Label>Message</Form.Label>
-            <Form.Control 
-              required
-              type="text" 
-              placeholder="Enter message" 
-            />
-          </Form.Group>
-          <Button variant="primary" type="submit">
-            Submit
-          </Button>
-        </Form> */}
+          validationSchema={schema}
+          initialValues={{
+              name: '',
+              email: '',
+              message: ''
+            }}
+          >
+            {({
+              handleChange,
+              values,
+              touched,
+              errors,
+            }) => (
+              <Form noValidate validated={validated} onSubmit={handleSubmit}>
+                  <Form.Group className="mb-3" as={Col} md="4" controlId="validationFormik01">
+                    <Form.Label>Name</Form.Label>
+                    <Form.Control
+                      type="text"
+                      name="name"
+                      placeholder="Enter name"
+                      value={values.name}
+                      onChange={handleChange}
+                      // isValid={touched.name && !errors.name}
+                      isInvalid={!!errors.name}
+                    />
+                  </Form.Group>
+                  <Form.Group className="mb-3" as={Col} md="4" controlId="validationFormik02">
+                    <Form.Label>Email</Form.Label>
+                    <Form.Control
+                      type="email"
+                      name="email"
+                      placeholder="Email address"
+                      value={values.email}
+                      onChange={handleChange}
+                      isInvalid={!!errors.email}
+                    />
+                    <Form.Control.Feedback type="invalid">
+                      {errors.email}
+                    </Form.Control.Feedback>
+                  </Form.Group>
+                  <Form.Group className="mb-3" as={Col} md="4" controlId="validationFormik03">
+                    <Form.Label>Message</Form.Label>
+                    <Form.Control
+                      type="text"
+                      name="message"
+                      placeholder="Enter message"
+                      value={values.message}
+                      onChange={handleChange}
+                      isInvalid={!!errors.message}
+                    />
+                    <Form.Control.Feedback type="invalid">
+                      {errors.message}
+                    </Form.Control.Feedback>
+                  </Form.Group>
+                  <Button variant="primary" type="submit" href={"mailto:" + values.email}>
+                    Submit
+                  </Button>
+              </Form>
+            )}
+        </Formik>
       </div>
       </Container>
       <Footer />
