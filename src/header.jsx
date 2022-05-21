1734ee60e75cc3db12fd8a5df340b619b64b45c4
JRM <johnrhmartin@gmail.com>
2022-05-20 22:57:06 -0400

    updated the header to track which page is active

--- src/header.jsx
+++ src/header.jsx
@@ -1,27 +1,66 @@
-import { Navbar, Nav, Col } from 'react-bootstrap';
-import { Link } from "react-router-dom";
+import { Navbar, Col } from 'react-bootstrap';
+import { NavLink } from "react-router-dom";
 import name from './assets/name_logo.png';
 
 export const Header = () => {
     return (
         <div className="header">
             <Navbar bg="dark" variant="dark" className="App-header">
-                <Navbar.Brand href="/">
-                    <img
-                        src={name}
-                        width="50"
-                        height="50"
-                        className="d-inline-block align-top App-header-brand"
-                        alt="React Bootstrap logo"
-                    />
-                </Navbar.Brand>
+                <Col>
+                <img
+                    src={name}
+                    width="50"
+                    height="50"
+                    className="d-inline-block align-top App-header-brand"
+                    alt="React Bootstrap logo"
+                />
+                </Col>
                 <Col className="App-header-nav">
-                    <Nav>
-                        <Link to="/" className="App-link">About Me</Link>
-                        <Link to="/portfolio" className="App-link">Portfolio</Link>
-                        <Link to="/Contact" className="App-link">Contact</Link>
-                        <Link to="/Resume" className="App-link">Resume</Link>
-                    </Nav>
+                <NavLink
+                    to="/"
+                    style={({ isActive }) => ({
+                        color: isActive ? '#000000' : '#ffffff',
+                        background: isActive ? '#92edf3' : '',
+                        borderRadius: '10px',
+                    })}
+                    className="App-link"
+                    >
+                    About Me
+                </NavLink>
+                <NavLink
+                    to="/portfolio"
+                    style={({ isActive }) => ({
+                        color: isActive ? '#000000' : '#ffffff',
+                        background: isActive ? '#92edf3' : '',
+                        borderRadius: '10px',
+                    })}
+                    className="App-link"
+                    >
+                    Portfolio
+                </NavLink>
+                <NavLink
+                    to="/contact"
+                    style={({ isActive }) => ({
+                        color: isActive ? '#000000' : '#ffffff',
+                        background: isActive ? '#92edf3' : '',
+                        borderRadius: '10px',
+                    })}
+                    className="App-link"
+                    >
+                    Contact
+                </NavLink>
+                <NavLink
+                    to="/Resume"
+                    style={({ isActive }) => ({
+                        color: isActive ? '#000000' : '#ffffff',
+                        background: isActive ? '#92edf3' : '',
+                        borderRadius: '10px',
+                    })}
+                    className="App-link"
+                    >
+                    Resume
+                </NavLink>
+                
                 </Col>
             </Navbar>
         </div>
