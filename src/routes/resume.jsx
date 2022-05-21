c506bfff567df071b25798b8f92c1cd649afb2d7
JRM <johnrhmartin@gmail.com>
2022-05-20 21:12:21 -0400

    added about 13 new image assets and updated the resume page

--- src/routes/resume.jsx
+++ src/routes/resume.jsx
@@ -4,6 +4,19 @@ import { Footer } from '../footer';
 import { Header } from '../header';
 import resume from '../assets/CV_JM.pdf';
 import photo from '../assets/pdf_icon.png';
+import node from '../assets/node.png';
+import mongo from '../assets/mongo.png';
+import mongoose from '../assets/mongoose.png';
+import mysql2 from '../assets/mysql.png';
+import seq from '../assets/sequelize.png';
+import npm from '../assets/npm.png';
+import heroku from '../assets/heroku.png';
+import insomnia from '../assets/insomnia.png';
+import postman from '../assets/postman.png';
+import react from '../assets/react.png';
+import html from '../assets/html.png';
+import css from '../assets/css.png';
+import javascript from '../assets/javascript.png';
 
 class DownloadLink extends React.Component {
   render() {
@@ -19,6 +32,14 @@ export default function Resume() {
       backgroundColor: '#b7e0f2a9',
       borderRadius: 25,
       padding: '1rem'
+    },
+    cardImage: {
+      borderRadius: 24,
+      width: '5rem',
+      height: '5rem',
+      padding: '10px',
+      margin: 'auto',
+      opacity: '80%'
     }
   }
     return (
@@ -34,36 +55,33 @@ export default function Resume() {
             <Card style={styles.card}>
               <Card.Header as="h5" style={styles.card}>Back-end Proficiences</Card.Header>
               <Card.Body>
-                <ul>
-                  <li>Node.js</li>
-                  <li>MongoDB</li>
-                  <li>Mongoose ODM</li>
-                  <li>MySQL2</li>
-                  <li>Sequelize</li>
-                  <li>NPM Packages</li>
-                  <ul>
-                    <li>Express.js</li>
-                    <li>Inquirer</li>
-                    <li>Console.table</li>
-                  </ul>
-                  <li>Heroku / Heroku CLI</li>
-                  <li>Insomnia</li>
-                </ul>
+                <Row>
+                  <Col><Row><img src={node} style={styles.cardImage} className="pdf-icon" alt="" /></Row><Row className="resume-org">Node.js</Row></Col>
+                  <Col><Row><img src={mongo} style={styles.cardImage} className="pdf-icon" alt="" /></Row><Row className="resume-org">MongoDB</Row></Col>
+                  <Col><Row><img src={mongoose} style={styles.cardImage} className="pdf-icon" alt="" /></Row><Row className="resume-org">Mongoose</Row></Col>
+                  <Col><Row><img src={mysql2} style={styles.cardImage} className="pdf-icon" alt="" /></Row><Row className="resume-org">MySQL2</Row></Col>
+                  <Col><Row><img src={seq} style={styles.cardImage} className="pdf-icon" alt="" /></Row><Row className="resume-org">Sequelize</Row></Col>
+                  <Col><Row><img src={npm} style={styles.cardImage} className="pdf-icon" alt="" /></Row><Row className="resume-org">NPM</Row></Col>
+                  <Col><Row><img src={heroku} style={styles.cardImage} className="pdf-icon" alt="" /></Row><Row className="resume-org">Heroku</Row></Col>
+                  <Col><Row><img src={insomnia} style={styles.cardImage} className="pdf-icon" alt="" /></Row><Row className="resume-org">Insomnia</Row></Col>
+                  <Col><Row><img src={postman} style={styles.cardImage} className="pdf-icon" alt="" /></Row><Row className="resume-org">Postman</Row></Col>
+                </Row>
               </Card.Body>
             </Card>
             </Col>
+          </Row>
+          <div className="extra-spacing"></div>
+          <Row>
             <Col>
               <Card style={styles.card}>
                 <Card.Header as="h5" style={styles.card}>Front-end Proficiences</Card.Header>
                 <Card.Body>
-                  <ul>
-                    <li>React</li>
-                    <li>Javascript</li>
-                    <li>HTML</li>
-                    <li>Netlify</li>
-                    <li>Photoshop</li>
-                  </ul>
-                </Card.Body>
+                <Row>
+                  <Col><Row><img src={react} style={styles.cardImage} className="pdf-icon" alt="" /></Row><Row className="resume-org">React</Row></Col>
+                  <Col><Row><img src={html} style={styles.cardImage} className="pdf-icon" alt="" /></Row><Row className="resume-org">HTML</Row></Col>
+                  <Col><Row><img src={css} style={styles.cardImage} className="pdf-icon" alt="" /></Row><Row className="resume-org">CSS</Row></Col>
+                  <Col><Row><img src={javascript} style={styles.cardImage} className="pdf-icon" alt="" /></Row><Row className="resume-org">Javascript</Row></Col>
+                </Row>                </Card.Body>
               </Card>
             </Col>
           </Row>
